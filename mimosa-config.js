74d3b257201c3918b84209a18deacb2f6522ac8c
wiseallie <wiseallie@gmail.com>
2014-03-25 17:11:47 +0200

    angular instantiating the app

--- mimosa-config.js
+++ mimosa-config.js
@@ -1,34 +1,41 @@
 exports.config = {
-  "modules": [
-    "copy",
-    "server",
-    "jshint",
-    "csslint",
-    "require",
-    "minify-js",
-    "minify-css",
-    "live-reload",
-    "bower",
-    "coffeescript",
-    "less",
-    "html-templates"
-  ],
-  "server": {
-    "views": {
-      "compileWith": "html",
-      "extension": "html"
+    "modules": [
+        "copy",
+        "server",
+        "jshint",
+        "csslint",
+        "require",
+        "minify-js",
+        "minify-css",
+        "live-reload",
+        "bower",
+        "coffeescript",
+        "less",
+        "html-templates"
+    ],
+    "server": {
+        "views": {
+            "compileWith": "html",
+            "extension": "html"
+        },
+        "defaultServer": {
+            "enabled": true
+        }
     },
-    "defaultServer": {
-      "enabled": true
-    }
-  },
-  "bower": {
-      'copy':{
-          "unknownMainFullCopy" : true
-//            "mainOverrides": {
-//                "requirejs-domready": ["domReady.js"],
-//                "font-awesome": ["css", "fonts"]
-//            }
-      }
+    "bower": {
+        'bowerDir': {
+            'clean': false
+        },
+        'copy': {
+            'unknownMainFullCopy': true,
+            'mainOverrides': {
+                'requirejs-domready': ['domReady.js'],
+                'font-awesome': ['css/', 'fonts/'],
+                'bootstrap': ['dist/css/', 'dist/fonts/', 'dist/js/']
+            }
+        }
+    },
+    'copy': {
+        extensions: ["js", "css", "png", "jpg", "jpeg", "gif", "html", "eot", "svg", "ttf", "woff", "otf", "yaml", "kml", "ico", "htc", "htm", "json", "txt", "xml", "xsd", "map", "md", "mp4", "mp3", 'map']
     }
 }
